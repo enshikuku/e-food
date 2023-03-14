654bd6534601f74039b3b5112ece4891a20240f8
Shikuku39 <enhikuku39@gmail.com>
2023-03-14 14:55:07 +0300

    updates

--- js/main.js
+++ js/main.js
@@ -1,17 +1,32 @@
 const swiper = new Swiper('.swiper', {
     autoplay: {
-      delay : 7000,
-      disableOnInteraction: false,
+        delay : 7000,
+        disableOnInteraction: false,
     },
     loop: true,
   
     pagination: {
-      el: '.swiper-pagination',
-      clickable : true,
+        el: '.swiper-pagination',
+        clickable : true,
     },
   
     navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
     }
 });
+let mealTypes = document.querySelectorAll('.mealTypes')
+let meals = document.querySelectorAll('.meals')
+
+mealTypes.forEach(mealType => {
+    mealType.addEventListener('click', () => {
+        let meal = mealType.nextElementSibling
+        if (meal.style.display === 'none') {
+            meals.forEach(meal => meal.style.display = 'none')
+            mealTypes.forEach(mealType => mealType.firstElementChild.src = 'images/plus.png')
+            meal.style.display = 'block'
+        } else {
+            meal.style.display = 'none'
+        }
+    })
+})
