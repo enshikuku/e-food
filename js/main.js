a0c7b1e17279958667695b1669c402c2d98e9ec1
Shikuku39 <enhikuku39@gmail.com>
2023-03-15 16:53:30 +0300

    updates

--- js/main.js
+++ js/main.js
@@ -14,19 +14,57 @@ const swiper = new Swiper('.swiper', {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
     }
+})
+function closeNav() {
+    document.querySelector('#nav-check').checked = false
+}
+// Get all the pages and buttons
+const pages = document.querySelectorAll('.menuPage');
+const prevBtn = document.getElementById('menuPrevBtn');
+const nextBtn = document.getElementById('menuNextBtn');
+
+// Set the initial page
+let currentPage = 0;
+showPage(currentPage);
+
+// Add event listeners to the buttons
+prevBtn.addEventListener('click', () => {
+  if (currentPage > 0) {
+    currentPage--;
+    showPage(currentPage);
+  }
 });
+
+nextBtn.addEventListener('click', () => {
+  if (currentPage < pages.length - 1) {
+    currentPage++;
+    showPage(currentPage);
+  }
+});
+
+// Function to show the current page
+function showPage(pageNumber) {
+  // Hide all pages
+  pages.forEach((page) => {
+    page.classList.remove('active');
+  });
+
+  // Show the current page
+  pages[pageNumber].classList.add('active');
+}
+
 // let mealTypes = document.querySelectorAll('.mealTypes')
 // let meals = document.querySelectorAll('.meals')
 
-mealTypes.forEach(mealType => {
-    mealType.addEventListener('click', () => {
-        let meal = mealType.nextElementSibling
-        if (meal.style.display === 'none') {
-            meals.forEach(meal => meal.style.display = 'none')
-            mealTypes.forEach(mealType => mealType.firstElementChild.src = 'images/plus.png')
-            meal.style.display = 'block'
-        } else {
-            meal.style.display = 'none'
-        }
-    })
-})
+// mealTypes.forEach(mealType => {
+//     mealType.addEventListener('click', () => {
+//         let meal = mealType.nextElementSibling
+//         if (meal.style.display === 'none') {
+//             meals.forEach(meal => meal.style.display = 'none')
+//             mealTypes.forEach(mealType => mealType.firstElementChild.src = 'images/plus.png')
+//             meal.style.display = 'block'
+//         } else {
+//             meal.style.display = 'none'
+//         }
+//     })
+// })
