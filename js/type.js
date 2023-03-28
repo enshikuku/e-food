48078f24b3bc840a2cc524592a32cb1b67bed310
Shikuku39 <enhikuku39@gmail.com>
2023-03-28 13:48:16 +0300

    updates

--- js/type.js
+++ js/type.js
@@ -1,16 +1,16 @@
 let typed = new Typed(".auto-input", {
-    strings: [ "Appetizers...", "Entrees...", "Desserts...", "Beverages..."],
-    typeSpeed: 60,
-    backSpeed:30,
+    strings: [ "Appetizers", "Entrees", "Desserts", "Beverages"],
+    typeSpeed: 100,
+    backSpeed: 100,
     loop:true,
     backDelay: 3100,
     showCursor: true,
-    cursorChar: '.',
+    cursorChar: '|',
 })
 let typed2 = new Typed(".auto-input-2", {
     strings: [ "Samosas", "Mshikaki", "Kachumbari", "Mahamri"],
-    typeSpeed: 30,
-    backSpeed:30,
+    typeSpeed: 60,
+    backSpeed:60,
     loop:true,
     backDelay: 1100,
     showCursor: true,
@@ -18,8 +18,8 @@ let typed2 = new Typed(".auto-input-2", {
 })
 let typed3 = new Typed(".auto-input-3", {
     strings: [ "Nyama Choma", "Pilau", "Githeri", "Sukuma Wiki", "Irio"],
-    typeSpeed: 30,
-    backSpeed:30,
+    typeSpeed: 60,
+    backSpeed:60,
     loop:true,
     backDelay: 1100,
     showCursor: true,
@@ -27,8 +27,8 @@ let typed3 = new Typed(".auto-input-3", {
 })
 let typed4 = new Typed(".auto-input-4", {
     strings: [ "Mandazi", "Kaimati", "Mutura"],
-    typeSpeed: 30,
-    backSpeed:30,
+    typeSpeed: 60,
+    backSpeed:60,
     loop:true,
     backDelay: 1100,
     showCursor: true,
@@ -36,8 +36,8 @@ let typed4 = new Typed(".auto-input-4", {
 })
 let typed5 = new Typed(".auto-input-5", {
     strings: [ "Kenyan Tea", "Tusker Beer", "Mango Juice", "Masala Chai"],
-    typeSpeed: 30,
-    backSpeed:30,
+    typeSpeed: 60,
+    backSpeed:60,
     loop:true,
     backDelay: 1100,
     showCursor: true,
