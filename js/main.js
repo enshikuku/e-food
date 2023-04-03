ae2c8761e378905b0db9bda063ecb69d9e2e136f
Shikuku39 <enhikuku39@gmail.com>
2023-04-03 15:42:23 +0300

    updates

--- js/main.js
+++ js/main.js
@@ -18,3 +18,17 @@ const swiper = new Swiper('.swiper', {
 function closeNav() {
     document.querySelector('#nav-check').checked = false
 }
+const scrollToTopBtn = document.getElementById("scrollToTopBtn")
+
+window.onscroll = function() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        scrollToTopBtn.style.display = "block"
+    } else {
+        scrollToTopBtn.style.display = "none"
+    }
+}
+scrollToTopBtn.addEventListener("click", function() {
+    document.body.scrollTop = 0
+    document.documentElement.scrollTop = 0
+})
+  
